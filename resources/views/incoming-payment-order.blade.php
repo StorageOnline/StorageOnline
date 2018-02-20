ca8a7a21384967cc84ea9de4a3703d86103746c8
klinsky <klinsky88@gmail.com>
2018-02-20 18:56:45 +0200

    20.02
    Стили в менюшку добавил, выровнял таблицу в Товарах

--- resources/views/incoming-payment-order.blade.php
+++ resources/views/incoming-payment-order.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Приходный ордер</div>
+                    <div class="panel-heading text-center">Приходный ордер</div>
 
                     <div class="panel-body">
                         <a href="#modal" class="btn btn-success"  data-toggle="modal" onclick="clearIncomingModal()">Добавить</a>
