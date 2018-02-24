90f0c4b294837c1266d20963dbf1ceeeac00c399
kravadima <kravadima@gmail.com>
2018-02-24 23:49:52 +0200

    add new file localization

---
+++ resources/lang/ru/messages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'goods' => 'Товары',
+    'counterparties' => 'Склад',
+    'incoming_order' => 'Приходный ордер',
+    'outgoing_order' => 'Расходный ордер',
+    'report' => 'Отчет',
+    'reports' => 'Отчеты',
+    'storage' => 'Склад',
+    'storages' => 'Склады',
+    'user' => 'Пользователь',
+    'users' => 'Пользователи',
+    'log' => 'Журнал',
+];
