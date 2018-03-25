0179a155b9c7c446fb4d8883a11d830c542b8e3d
kravadima <kravadima@gmail.com>
2018-03-25 19:00:10 +0300

    связь компании и пользователя при создании компании

--- app/Model/Companies/Company.php
+++ app/Model/Companies/Company.php
@@ -3,13 +3,24 @@
 namespace App\Model\Companies;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
+    // для возможности "мягкого удаления"
+    use SoftDeletes;
+
     /*Имя таблицы в Базе Данных*/
     protected $table = 'company';
 
     public $timestamps = true;
 
     protected $fillable = ['*'];
+
+    /**
+     * Атрибуты, которые должны быть преобразованы в даты.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 }
--- app/Model/Companies/UserCompany.php
+++ app/Model/Companies/UserCompany.php
@@ -3,13 +3,29 @@
 namespace App\Model\Companies;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserCompany extends Model
 {
+    // для возможности "мягкого удаления"
+    use SoftDeletes;
+
     /*Имя таблицы в Базе Данных*/
     protected $table = 'user_company';
 
     public $timestamps = true;
 
     protected $fillable = ['*'];
+
+    /**
+     * Атрибуты, которые должны быть преобразованы в даты.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    public function relationCompany()
+    {
+        return $this->hasOne('App\Model\Companies\Company', 'id', 'company_id');
+    }
 }
